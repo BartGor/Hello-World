1fe8f710abcc811ba990219d32c0c12f48fd127c
BartGor <bartosz.gorny@hotmail.com>
2018-04-13 20:00:18 +0200

    Add add bmiCalc package; Create BMI calculator;

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
 
         System.out.println("Hello World!");
+
+        System.out.print("Wprowadz liczbę: ");
+        int luckyNum = userInput.nextInt();
+        System.out.println("Twoja liczba to: "+luckyNum);
+
     }
 }
---
+++ src/bmiCalc/BmiCalculator.java
@@ -0,0 +1,37 @@
+package bmiCalc;
+
+import java.util.Scanner;
+
+import static java.lang.Math.*;
+
+public class BmiCalculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Podaj wsrost: ");
+        double height = sc.nextDouble();   // wztrost w metrach
+        System.out.print("Podaj wagę: ");
+        double weight = sc.nextDouble();     //waga w kg
+
+        double bmi = weight / pow(height, 2);
+
+        System.out.println("Twój współczynnik BMI wynosi: " + String.format("%.2f", bmi));
+
+        if (bmi < 18.5) {
+            System.out.println("Twój stan to: Niedowaga.");
+        } else if (bmi >= 18.5 && bmi <= 24.99) {
+            System.out.println("Twój stan to: Norma.");
+        } else if (bmi >= 25 && bmi <= 29.99) {
+            System.out.println("Twój stan to: Nadwaga.");
+        } else if (bmi >= 30 && bmi <= 34.99) {
+            System.out.println("Twój stan to: Otyłość I stopnia.");
+        } else if (bmi >= 35 && bmi <= 39.99) {
+            System.out.println("Twój stan to: Otyłość II stopnia.");
+        } else if (bmi >= 40) {
+            System.out.println("Twój stan to: Otyłość III stopnia.");
+        } else {
+            System.out.println("Błąd w obliczeniach");
+        }
+
+    }
+}
